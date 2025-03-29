ff0379952775d99f96aaafd6dfb9638a911761f2
Diego <diegodelacruz775@gmail.com>
2025-03-29 14:01:59 -0600

    Recuperacion de metodo registrar

--- PHP/operaciones.php
+++ PHP/operaciones.php
@@ -37,9 +37,33 @@ if (isset($_POST['login'])) {
     }
 }
 
-// Procesar Registro
 if (isset($_POST['registrar'])) {
-    // ... (tu código de registro existente)
+    $carnet = $_POST['carnet'];
+    $nombres = $_POST['nombres'];
+    $apellidos = $_POST['apellidos'];
+    $password = $_POST['password'];
+    $celular = $_POST['celular'];
+    $email = $_POST['email'];
+    $foto = $_POST['foto'];
+    $tipouser = (int) $_POST['tipouser'];
+    $carrera = (int) $_POST['carrera'];
+    $seccion = $_POST['seccion'];
+
+    // Verificar que el campo no esté vacío
+    if (!empty($nombres) && !empty($apellidos) && !empty($password) && !empty($celular) && !empty($email)
+        && !empty($foto) && !empty($tipouser) && !empty($carrera) && !empty($seccion) && !empty($carnet)) { 
+        $user_id = 1;
+        $stmt = $conexion->prepare("CALL RegistrarUsuarios(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssssssiis", $carnet, $nombres, $apellidos, $password, $celular, $email, $foto, $tipouser, $carrera, $seccion);
+        $stmt->execute();
+
+        // Cerrar conexión
+        $stmt->close();
+
+        echo "Usuario insertado correctamente.";
+    } else {
+        echo "Por favor, rellenar todos los campos.";
+    }
 }
 
 mysqli_close($conexion);
